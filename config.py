8270f51ccee62e81c4f67a78cffda7ec0fb3f46b
pk8429528-hub <pk8429528@gmail.com>
2026-05-29 09:27:24 +0530

    Update ASSUSERNAME and SHASHANK_IMG URLs

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "your_aadiii")
 BOT_USERNAME = getenv("BOT_USERNAME", "@aadimusicrobot")
 BOT_NAME = getenv("BOT_NAME", "SAPNA")
-ASSUSERNAME = getenv("ASSUSERNAME", "KHWAAISH_HOON")
+ASSUSERNAME = getenv("ASSUSERNAME", "aadiXassistant")
 
 # MongoDB
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
@@ -111,11 +111,11 @@ SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/aesldg.jpg"
 
 
 SHASHANK_IMG = [
-    "https://files.catbox.moe/dw0as6.jpg",
-    "https://files.catbox.moe/t2m1pv.jpg",
-    "https://files.catbox.moe/lsbotb.jpg",
-    "https://files.catbox.moe/huuy1f.jpg",
-    "https://files.catbox.moe/7vfivr.jpg",
+    "https://files.catbox.moe/8r7k6f.jpg",
+    "https://files.catbox.moe/vl8ad7.jpg",
+    "https://files.catbox.moe/ihzn6r.jpg",
+    "https://files.catbox.moe/xl9xtz.jpg",
+    "https://files.catbox.moe/r1k8hb.jpg",
     "https://files.catbox.moe/dqtuv2.jpg",
     "https://files.catbox.moe/ac3tzn.jpg"
 ]
